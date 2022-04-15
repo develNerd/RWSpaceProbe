507ac19f485a9a943f8af3593836bfb42a3056ca
Isaac <isaac@farmerline.co>
2022-04-15 12:35:29 +0000

    Initial

--- app/src/main/java/com/raywenderlich/android/rwandroidtutorial/model/ProbeMission.kt
+++ app/src/main/java/com/raywenderlich/android/rwandroidtutorial/model/ProbeMission.kt
@@ -4,4 +4,4 @@ package com.raywenderlich.android.rwandroidtutorial.model
  * @author Isaac Akakpo
  * Created on 4/14/2022 11:27 AM
  */
-data class ProbeMission(val probeName:String,val missionDescription:String)
+data class ProbeMission(val probeName:String,val missionDescription:String )
